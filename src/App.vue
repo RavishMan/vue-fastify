9f0b5a03ff23ce9e0f8077395fcbef19f6f26c55
RavishMan <ravishfeed@gmail.com>
2021-04-02 20:20:32 +0500

    Добавлен примеры

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <v-app>
     <v-main>
-      <div class="container">
-        <div class="row">
-          <div class="col col-2">1</div>
-          <div class="col col-2">2</div>
-          <div class="col col-2">3</div>
-          <div class="col col-2">4</div>
-          <div class="col col-2">5</div>
-        </div>
-      </div>
-
       <v-container>
         <v-row>
           <v-col cols="2">1</v-col>
@@ -21,73 +11,102 @@
         </v-row>
       </v-container>
 
-      <v-container>
+      <v-container fluid>
         <v-row justify="space-around">
-          <v-card width="400" v-for="i in 5">
-            <v-img
-                height="200px"
-                src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+          <v-col
+              cols="auto"
+              xs="12"
+              lg="2"
+              xl="2"
+              v-for="i in 12"
+          >
+            <v-card
+                class="mx-auto"
+                max-width="344"
             >
-              <v-app-bar
-                  flat
-                  color="rgba(0, 0, 0, 0)"
-              >
-                <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
+              <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                  height="200px"
+              ></v-img>
 
-                <v-toolbar-title class="title white--text pl-0">
-                  Messages
-                </v-toolbar-title>
-
-                <v-spacer></v-spacer>
-
-                <v-btn
-                    color="white"
-                    icon
-                >
-                  <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
-              </v-app-bar>
-
-              <v-card-title class="white--text mt-8">
-                <v-avatar size="56">
-                  <img
-                      alt="user"
-                      src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
-                  >
-                </v-avatar>
-                <p class="ml-3">
-                  John Doe
-                </p>
+              <v-card-title>
+                Протеины
               </v-card-title>
-            </v-img>
 
-            <v-card-text>
-              <div class="font-weight-bold ml-8 mb-2">
-                Today
-              </div>
-
-              <v-timeline
-                  align-top
-                  dense
-              >
-                <v-timeline-item
-                    v-for="message in messages"
-                    :key="message.time"
-                    :color="message.color"
-                    small
-                >
-                  <div>
-                    <div class="font-weight-normal">
-                      <strong>{{ message.from }}</strong> @{{ message.time }}
-                    </div>
-                    <div>{{ message.message }}</div>
-                  </div>
-                </v-timeline-item>
-              </v-timeline>
-            </v-card-text>
-          </v-card>
+            </v-card>
+          </v-col>
         </v-row>
       </v-container>
+
+      <div class="text-center">
+        <v-dialog
+            v-model="dialog"
+            width="500"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                color="red lighten-2"
+                dark
+                v-bind="attrs"
+                v-on="on"
+            >
+              Нажми на меня
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title class="headline grey lighten-2">
+             текст больше
+            </v-card-title>
+
+            <v-card-text>
+              <v-text-field
+                  v-model="firstname"
+                  :rules="nameRules"
+                  :counter="10"
+                  label="First name"
+                  required
+              ></v-text-field>
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="primary"
+                  text
+                  @click="closeModal"
+              >
+                I accept
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
+
+      <v-carousel :interval="500" :cycle="true" :continuous="true" v-model="model">
+        <v-carousel-item
+            v-for="(color, i) in colors"
+            :key="color"
+        >
+          <v-sheet
+              :color="color"
+              height="100%"
+              tile
+          >
+            <v-row
+                class="fill-height"
+                align="center"
+                justify="center"
+            >
+              <div class="display-3">
+                Slide {{ i + 1 }}
+              </div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
     </v-main>
   </v-app>
 </template>
@@ -101,24 +120,53 @@ export default {
     //
     messages: [
       {
-        from: 'You',
+        from:    'You',
         message: `12312312`,
-        time: '10:42am',
-        color: 'deep-purple lighten-1',
+        time:    '10:42am',
+        color:   'deep-purple lighten-1',
       },
       {
-        from: 'John Doe',
+        from:    'John Doe',
         message: 'Yeah, sure. Does 1:00pm work?',
-        time: '10:37am',
-        color: 'green',
+        time:    '10:37am',
+        color:   'green',
       },
       {
-        from: 'You',
+        from:    'You',
         message: 'Did you still want to grab lunch today?',
-        time: '9:47am',
-        color: 'deep-purple lighten-1',
+        time:    '9:47am',
+        color:   'deep-purple lighten-1',
       },
     ],
+    model: 0,
+    firstname: '',
+    colors: [
+      'primary',
+      'secondary',
+      'yellow darken-2',
+      'red',
+      'orange',
+    ],
+    dialog: false,
+    nameRules: [
+      v => !!v || 'Имя обязательно',
+      v => v.length <= 10 || 'Меньше 10 символов введи',
+    ],
   }),
+  methods: {
+    logvmodel(){
+      console.log(this.firstname)
+    },
+    closeModal(){
+      this.dialog = false
+      this.logvmodel()
+    }
+  }
 };
 </script>
+
+<style>
+.v-sheet.v-card:not(.v-sheet--outlined) {
+  box-shadow: none !important;
+}
+</style>
